ba9b2a3821d3cb194f36a5af0ee3c5a0a8536b2c
snowman07 <domingo.arrbelrey@gmail.com>
2023-12-27 12:00:18 -0700

    sample code for encapsulation

---
+++ Oop/Encapsulation/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Encapsulation;
+
+Person person = new Person();
+person.Name = "Testname";
+Console.WriteLine(person.Name);
+
