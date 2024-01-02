b79ae572bb7bd5af742fa5ed010b905a9c9f7791
snowman07 <domingo.arrbelrey@gmail.com>
2024-01-01 19:43:41 -0700

    Code for abstraction OOP

---
+++ Oop/Abstraction/Animal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraction
+{
+    abstract class Animal
+    {
+        // abstract method
+        public abstract void animalSound();
+
+        // regular method
+        public void sleep() 
+        {
+            Console.WriteLine("ZZZZ");
+        }
+    }
+}
---
+++ Oop/Abstraction/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using Abstraction;
+
+Pig pig = new Pig();
+pig.animalSound();
+pig.sleep();
