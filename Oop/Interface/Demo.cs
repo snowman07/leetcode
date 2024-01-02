1685d5e53ea936cb21a2298d133d83733328a465
snowman07 <domingo.arrbelrey@gmail.com>
2024-01-01 20:08:05 -0700

    Code for Interface

---
+++ Oop/Interface/Demo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    class Demo : IFirstInterface, ISecondInterface
+    {
+        public void methodFromFirstInterface()
+        {
+            Console.WriteLine("Implementation from first interface");
+        }
+
+        public void methodFromSecondInterface()
+        {
+            Console.WriteLine("This is the second interface implementation.");
+        }
+    }
+}
---
+++ Oop/Interface/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using Interface;
+
+Demo myDemo = new Demo();
+myDemo.methodFromFirstInterface();
+myDemo.methodFromSecondInterface();
