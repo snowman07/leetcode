f29f8fad772e56dce01e0929df13cf0007a553a6
snowman07 <domingo.arrbelrey@gmail.com>
2024-01-19 19:20:46 -0700

    List of Objects in c#

---
+++ CSharpLearning/ListOfObjects/Player.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListOfObjects
+{
+    public class Player
+    {
+        public string username;
+        public Player(string username)
+        {
+
+            this.username = username;
+
+        }
+
+        public override string ToString()
+        {
+            return username;
+        }
+    }
+}
---
+++ CSharpLearning/ListOfObjects/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//REFERENCE: https://www.youtube.com/watch?v=aLhAmimoQj8
+
+//Console.WriteLine("Hello, World!");
+
+using ListOfObjects;
+
+List<Player> players = new List<Player>();
+
+//Player player1 = new Player("First");
+//Player player2 = new Player("Second");
+//Player player3 = new Player("Third");
+
+players.Add(new Player("First"));
+players.Add(new Player("Second"));
+players.Add(new Player("Third"));
+
+foreach (Player p in players)
+{
+    //Console.WriteLine(p.username);
+
+    //override ToString 
+    Console.WriteLine(p);
+
+}
+
