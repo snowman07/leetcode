27e2413c75ecd1d315bf302a384d589f9edd2fed
snowman07 <domingo.arrbelrey@gmail.com>
2024-02-09 21:49:23 -0700

    solution for Contains Duplicate

--- LeetCodeProblems/217ContainsDuplicate/Program.cs
+++ LeetCodeProblems/217ContainsDuplicate/Program.cs
@@ -1,2 +1,38 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
+
+
+// Hard-coded array input
+
+int[] nums = { 1, 2, 3, 4, 1 };
+
+bool containsDuplicate = ContainsDuplicate(nums);
+
+if (containsDuplicate)
+{
+    Console.WriteLine("TRUE. Input contains duplicate.");
+}
+else
+{
+    Console.WriteLine("FALSE. Input does not contain duplicate.");
+}
+
+bool ContainsDuplicate(int[] nums)
+{
+    // Use a HashSet to keep track of unique elements encounteres so far
+    HashSet<int> set = new HashSet<int>();
+
+    foreach (int x in nums)
+    {
+        // If the element is already in the set, it's a duplicate
+        if (set.Contains(x))
+        {
+            return true;
+        }
+        // Add the element to the set
+        set.Add(x);
+    }
+
+    // No duplicates found
+    return false;
+}
